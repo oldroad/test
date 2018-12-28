d80f2ad960daf8382c42310de691bb64bf2c1809
old <old_road@qq.com>
2018-12-28 14:52:31 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,4 +19,13 @@ pipeline {
             }
         }
     }
+    
+    post {
+        echo '发送邮件'
+        always {
+        mail to: 'old_road@qq.com',
+             subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+             body: "Something is wrong with ${env.BUILD_URL}"
+            }
+        }
 }
