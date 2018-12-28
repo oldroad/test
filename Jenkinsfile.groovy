eb600e53ac52967fd67421527286b5573ebe7624
old <old_road@qq.com>
2018-12-28 14:54:24 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -21,9 +21,9 @@ pipeline {
     }
     
     post {
-        echo '发送邮件'
         always {
-        mail to: 'old_road@qq.com',
+             echo '发送邮件'
+             mail to: 'old_road@qq.com',
              subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
              body: "Something is wrong with ${env.BUILD_URL}"
             }
